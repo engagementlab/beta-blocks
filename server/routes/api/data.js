ac8715041b9b1130cd350ca7fb7107d159ee43fd
Johnny Richardson <johnny.c.rich@gmail.com>
2019-03-21 17:47:17 -0400

    Dictionary cleanup for labels, census bin normalization, test for replacing census vars with labels if available in dict.

--- server/routes/api/data.js
+++ server/routes/api/data.js
@@ -44,7 +44,8 @@ exports.set = function (req, res) {
 
     const request = require('request'),
           dictionary = require('../../dictionary');
-    let keys = _.keys(dictionary);
+          
+        //   console.log(dictionary['b01001008'])
 
     request(
         'https://res.cloudinary.com/engagement-lab-home/raw/upload/beta-blocks/data.geojson'
@@ -55,24 +56,39 @@ exports.set = function (req, res) {
                 _.each(parsed.features, (feat) => {
 
                     let props = feat.properties;
+                    let censusKeys = _.filter(_.keys(props), (prop) => {
+                        return prop.indexOf('census_') === 0;
+                    });
 
+                    // Precision reduction
                     _.each(feat.geometry.coordinates[0], (coord) => {
 
                         coord[0] = coord[0].toPrecision(6);
                         coord[1] = coord[1].toPrecision(6);
 
                     });
-                    
-                    props.sidewalk = Math.round(props.sidewalks_area_score/props.sidewalks_area);
-                    props.pop = props.census_b01003001 * (props._area/props.census_area);
                     props.origin = [props.origin[0].toPrecision(6), props.origin[1].toPrecision(6)]
                     
-                    // delete props.sidewalks_area_score;
-                    // delete props.sidewalks_area;
-                    // delete props.sidewalks_score;
-                    // delete props.sidewalks_count;
-                    delete props.census_area;
-                    delete props._area;
+                    // Normalize
+                    props.sidewalk = Math.round(props.sidewalks_area_score/props.sidewalks_area);
+                    
+                    _.each(censusKeys, (key) => {
+                        /* if(dictionary[key.replace('census_', '')])
+                            props[dictionary[key.replace('census_', '')]] = props[key] * (props._area/props.census_area);
+                        else */
+                            props[key.replace('census_', '')] = props[key] * (props._area/props.census_area);
+
+                        // props[key+'_norm'] = props[key] * (props._area/props.census_area);
+                        // ps.census_b01003001 = props.census_b01003001 * (props._area/props.census_area);
+                        delete props[key];
+                    });
+                                        
+                    delete props.sidewalks_area_score;
+                    delete props.sidewalks_area;
+                    delete props.sidewalks_score;
+                    delete props.sidewalks_count;
+                    // delete props.census_area;
+                    // delete props._area;
                     
                     /* _.each(keys, (key) => {
                         let value = feat.properties['census_'+key];
