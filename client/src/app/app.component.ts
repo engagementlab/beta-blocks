e753646c9fe1dc491c23e03287b22b5bbe620dcc
Johnny Richardson <johnny.c.rich@gmail.com>
2019-04-10 15:08:10 -0400

    Fleshed out tech explorers layout, QA banner added, several small fixes.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
+  public isQABuild: boolean;
   title = 'Beta Blocks';
+
+constructor(private _router: Router, private _titleSvc: Title) {
+
+  this.isQABuild = environment.qa;
+  this._titleSvc.setTitle((this.isQABuild ? '(QA) ' : '') + this.title);
+
+ }
+
+ ngOnInit() {
+
+  this._router.events.subscribe((evt) => {
+
+    if (!(evt instanceof NavigationEnd)) {
+      return;
+    }
+
+    // Always go to top of page
+    window.scrollTo(0, 0);
+
+  });
+
+ }
+
 }
--- client/src/app/zones/zones.component.ts
+++ client/src/app/zones/zones.component.ts
@@ -13,6 +13,8 @@ export class ZonesComponent implements OnInit {
   public hasContent: boolean;
   public content: any;
 
+  public locationIndices: Number[] = [1, 2, 3];
+
   constructor(private _dataSvc: DataService, private _scrollToService: ScrollToService) {
 
     _dataSvc.getDataForUrl('/api/zones/get').subscribe((data: any) => {
@@ -25,6 +27,28 @@ export class ZonesComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  getLocationInfo(i: number) {
+    
+    return {
+      name: this.content['location' + i + 'Name'],
+      description: this.content['location' + i + 'Description'],
+      address: this.content['location' + i + 'Address']
+    };
+
+  }
+
+  goToZone(i: number) {
+
+    this._scrollToService
+      .scrollTo({
+        target: document.getElementById('zone' + i),
+        offset: 0,
+        easing: 'easeOutQuint',
+        duration: 700
+      });
+
   }
 
 }
