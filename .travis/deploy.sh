cd0cb93695cd9a23a1fa9759b041c25c1eb18601
Johnny Richardson <johnny.c.rich@gmail.com>
2019-04-26 16:01:42 -0400

    Travis CI script fix.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Ensure in root and run deploy after logging in
-ssh -i ../travis_rsa deploy@"$1" "cd /srv/beta-blocks/source && git pull origin master && sh deploy.sh qa"
+ssh -i travis_rsa deploy@"$1" "cd /srv/beta-blocks/source && git pull origin master && sh deploy.sh qa"
