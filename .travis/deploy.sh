2ecdb22c2011bee524fb551a69381748cea846eb
Johnny Richardson <johnny.c.rich@gmail.com>
2019-04-26 15:45:14 -0400

    Zones fix, CI config fix.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-ssh deploy@"$1";
-git pull origin master;
-sh deploy.sh qa;
+# Ensure in root and run deploy after logging in
+ssh deploy@"$1" "cd /srv/beta-blocks/source && git pull origin master && sh deploy.sh qa"
