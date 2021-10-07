2df10441d35e69a601efa50bd9d2c30c83d2fbae
Johnny Richardson <johnny.c.rich@gmail.com>
2021-10-07 12:30:50 -0400

    feat: static discover build.

---
+++ discover/build/precache-manifest.f129b1cc2f62e8bd2cf2fb58f5da6b78.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "9523ad432eafe0990316",
+    "url": "/static/css/main.014a8b3b.chunk.css"
+  },
+  {
+    "revision": "9523ad432eafe0990316",
+    "url": "/static/js/main.51b4196c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "754015d16da6320539d5",
+    "url": "/static/css/2.83e852f6.chunk.css"
+  },
+  {
+    "revision": "754015d16da6320539d5",
+    "url": "/static/js/2.dcf634d9.chunk.js"
+  },
+  {
+    "revision": "d92651c5cdf66a806052f36585068ad8",
+    "url": "/static/media/pin.d92651c5.png"
+  },
+  {
+    "revision": "f380b7c983ab52d654856bc769b4f7d2",
+    "url": "/static/media/Labels.f380b7c9.svg"
+  },
+  {
+    "revision": "87d90d28bc5c7cc9fda2abd868312eb5",
+    "url": "/static/media/valkyrie_t3_regular.87d90d28.woff"
+  },
+  {
+    "revision": "eb6655c6852fb2625f4a9bd01950745d",
+    "url": "/static/media/valkyrie_t3_bold_italic.eb6655c6.woff"
+  },
+  {
+    "revision": "1e7e0239c3aa982d5d269a8989820526",
+    "url": "/static/media/valkyrie_t3_bold.1e7e0239.woff"
+  },
+  {
+    "revision": "7b5f3936813d22418589abc6807f5c9a",
+    "url": "/static/media/valkyrie_t4_regular.7b5f3936.woff"
+  },
+  {
+    "revision": "e3fd2d260b180d0dd1c79410ddeac7b4",
+    "url": "/static/media/valkyrie_t4_italic.e3fd2d26.woff"
+  },
+  {
+    "revision": "4ab8e48baf3bddbeb18fbd84824a2b02",
+    "url": "/static/media/valkyrie_t4_bold.4ab8e48b.woff"
+  },
+  {
+    "revision": "99c8294c043424da8b0229a223d81b92",
+    "url": "/static/media/valkyrie_t4_bold_italic.99c8294c.woff"
+  },
+  {
+    "revision": "ef654fe3c959d16ef605a7dc070e10d0",
+    "url": "/static/media/valkyrie_c3_bold.ef654fe3.woff"
+  },
+  {
+    "revision": "f2141650b3bffbea4eed61b51b847347",
+    "url": "/static/media/valkyrie_c4_regular.f2141650.woff"
+  },
+  {
+    "revision": "0bf686a679274fddfd6b1612099c05af",
+    "url": "/static/media/valkyrie_c4_bold.0bf686a6.woff"
+  },
+  {
+    "revision": "4e1f9cdf594f333c4569137500e6e763",
+    "url": "/static/media/valkyrie_c3_regular.4e1f9cdf.woff"
+  },
+  {
+    "revision": "7fad66a14484812c3490a771a2b73572",
+    "url": "/static/media/Hover Details.7fad66a1.svg"
+  },
+  {
+    "revision": "773fcf187b361194c17f0456f015255d",
+    "url": "/index.html"
+  }
+];
